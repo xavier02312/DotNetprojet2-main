a66482f237b9160254b097f5291cf76fdb9afe0b
xavier02312 <paqueriaudxavier@gmail.com>
2023-11-09 08:32:11 +0100

    AddPanier

--- P2FixAnAppDotNetCode/Models/Repositories/IProductRepository.cs
+++ P2FixAnAppDotNetCode/Models/Repositories/IProductRepository.cs
@@ -1,8 +1,15 @@
-﻿namespace P2FixAnAppDotNetCode.Models.Repositories
+﻿using System.Collections.Generic;
+// Appel using pour List<Product>
+
+
+namespace P2FixAnAppDotNetCode.Models.Repositories
 {
     public interface IProductRepository
     {
-        Product[] GetAllProducts();
+        List<Product> GetAllProducts();
+
+        //Ajout méthode getProductById
+        Product GetProductById(int id);
 
         void UpdateProductStocks(int productId, int quantityToRemove);
     }
